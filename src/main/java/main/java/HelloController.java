5e85e596921d082f9893166cd1e9a9f9d5f864a1
sparhawk2020 <cjjc20001@yahoo.com>
2021-10-21 10:44:32 -0700

    Initial commit

---
+++ src/main/java/main/java/HelloController.java
@@ -0,0 +1,33 @@
+package main.java;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String printHelloWorld1(ModelMap modelMap){
+
+        // add attribute to load modelMap
+        modelMap.addAttribute("message",
+                "Hello World and Welcome to Spring MVC!");
+
+        // return the name of the file to be loaded "hello_world.jsp"
+        return "hello_world";
+    }
+
+    @RequestMapping(value = "/hello_world", method = RequestMethod.GET)
+    public String printHelloWorld(ModelMap modelMap){
+
+        // add attribute to load modelMap
+        modelMap.addAttribute("message",
+                "Hello World and Welcome to Spring MVC!");
+
+        // return the name of the file to be loaded "hello_world.jsp"
+        return "hello_world";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello_world.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cjjc2
+  Date: 2021-10-19
+  Time: 9:30 p.m.
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Spring MVC Demo</title>
+</head>
+<body>
+<h2>${message}</h2>
+</body>
+</html>
